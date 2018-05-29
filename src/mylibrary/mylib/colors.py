2549787ff7fb0ff5b51c938bbf5f18e0d380d14a
Jonathan Garcia-Mallen <jgarciamallen@sea-machines.com>
2018-05-29 12:09:07 -0400

    [multi-py] made snapcraft and a tiny colors library

---
+++ src/mylibrary/mylib/colors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# https://en.wikipedia.org/wiki/ANSI_escape_code#3/4_bit
+
+color_escapes = {
+    "red" : "\033[31m",
+    "green" : "\033[32m",
+    "yellow" : "\033[33m",
+    "blue" : "\033[34m",
+    "magenta" : "\033[35m",
+    "cyan" : "\033[36m",
+    }
