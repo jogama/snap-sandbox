55fa152f8cdaa806f34b186f1d0bc444174cfec5
Jonathan Garcia-Mallen <jgarciamallen@sea-machines.com>
2018-05-29 13:54:08 -0400

    [multi-py] added hello_es, for spanish greeting

---
+++ src/spanish/hello/utils.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def greet(escape, message):
+    print(escape + message)
+
---
+++ src/spanish/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/ python3
+
+from setuptools import setup, find_packages
+
+setup(
+    ### Metadata
+    name='hello_es',
+
+    ### Dependencies
+    install_requires=[
+        'mylib',
+    ],
+
+    ### Interface
+    entry_points={
+        'console_scripts': [
+            # excecutable name = package.module:function
+            'hello_es = hello:hello:main'
+        ]
+    },
+
+    ### Contents
+    packages=find_packages(exclude=['test*']),
+)
