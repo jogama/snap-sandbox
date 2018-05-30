09a66087bbd5cb8d2eee7e71727a00ff14d4e43d
Jonathan Garcia-Mallen <jgarciamallen@sea-machines.com>
2018-05-30 19:48:07 -0400

    [multi-py] add setuptools as pylib dependency. Call pymodules in c++
    The setuptools line did not get the setuptools module into snap/parts,
    not in prime. The final snap fails accordingly.

--- src/cpp/caller.cpp
+++ src/cpp/caller.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cstdlib> // system
 
 using namespace std;
 int main(){
   cout << "Who is here?\n";
+  //  system("python3 -c 'print('Python3 is!')'");
+  system("hello_en");
+  system("hello_es");
+  system("hello_zh");
+
+  return 0; 
 }
 
